a9c546359c6b6f897756c3edfba239069b89d026
Andrea Herbas <andrea.herbas.dev@gmail.com>
2016-03-15 10:32:57 -0400

    Initial project

---
+++ SwiftPickerCell/SwiftPickerCell.h
@@ -0,0 +1,19 @@
+//
+//  SwiftPickerCell.h
+//  SwiftPickerCell
+//
+//  Created by Andrea Herbas on 3/15/16.
+//  Copyright © 2016 Andrea Herbas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftPickerCell.
+FOUNDATION_EXPORT double SwiftPickerCellVersionNumber;
+
+//! Project version string for SwiftPickerCell.
+FOUNDATION_EXPORT const unsigned char SwiftPickerCellVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftPickerCell/PublicHeader.h>
+
+
